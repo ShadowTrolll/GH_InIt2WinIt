1de3b1ddbf5f6147d0920abc271f5a5cfda3a13c
lnotspotl <lnotspotl@gmail.com>
2023-06-02 15:43:28 +0200

    create dataset for single meter

--- data_prompts.py
+++ data_prompts.py
@@ -19,6 +19,27 @@ class meauserement:
         self.importance = importance 
         self.time = time
 
+def create_dataset(meter_id = None):
+    """
+        dataset = [
+            (importance_1, time_in_minutes_1),
+            (importance_2, time_in_minutes_2),
+            (importance_3, time_in_minutes_3),
+            ...
+        ]
+    """
+    dataset = []
+    if meter_id is not None:
+        assert isinstance(meter_id, int)
+        data = get_meter_data(meter_id)
+        for data_point in data:
+            dataset.append(
+                (data_point["importance"], time2minute(data_point["time"]))
+            )
+    else:
+        raise NotImplementedError
+    return dataset
+
 def get_meter_data(meter_id = None):
     response = None
     if meter_id is not None:
